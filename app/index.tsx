c968ca7795b18b4bd4a1d8df6af8118720a7616b
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-08-03 14:38:53 +0300

    chore: Add NativeWind configuration and dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
-
+import { StatusBar } from "expo-status-bar";
 const App = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
