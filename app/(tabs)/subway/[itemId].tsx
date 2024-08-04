8498e366703de8fedf0872589197a0e9de977ac7
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-08-04 16:54:00 +0300

    refactor: Remove unused SubwayList component and update subway related files

--- app/(tabs)/subway/[itemId].tsx
+++ app/(tabs)/subway/[itemId].tsx
@@ -9,26 +9,22 @@ import ListItem from "@/components/ListItem";
 const Subway = () => {
   const { itemId } = useLocalSearchParams();
   const subway = subwayLines.find((line) => line.name === itemId);
-
   return (
     <GradientBlur>
       <SafeView isMain={false}>
         <View className="p-4">
           <Pressable
             onPress={() => router.back()}
-            className="flex-row items-center"
+            className="flex-row items-center bg-white p-2 rounded-full w-[50px] justify-center"
           >
-            <Ionicons name="arrow-back" size={36} color="white" />
-            <Text className="text-white text-center text-xl pl-2">
-              Back to list
-            </Text>
+            <Ionicons name="arrow-back" size={32} color="black" />
           </Pressable>
           <View className="pt-4">
             <Text className="text-white text-center font-bold text-3xl">
               {itemId}
             </Text>
             <Text className="text-white text-center text-lg mb-2">
-              Subway line details
+              {subway?.title}
             </Text>
             <FlatList
               data={subway?.stations}
